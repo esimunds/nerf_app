a085c0b265549fe2ccea33d170bd45e9df84e01d
Erik Simunds <erik.simunds@gmail.com>
2022-03-17 23:45:55 -0700

    Removed scaffold
    Removed scaffold so I can build the app one step at a time

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_063333) do
-
-  create_table "players", force: :cascade do |t|
-    t.string "name"
-    t.integer "wins"
-    t.integer "losses"
-    t.boolean "alive"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
